aa855ce829b3ee14d35c5ce96903409e530f65c5
Ilya <ilua107@gmail.com>
2021-07-07 21:26:18 +0300

    LE: 977. Squares of a Sorted Array

---
+++ SWGeneral/Tasks/LeetCode/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,43 @@
+/*
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        std::vector<int> result(nums.size());
+        int i = 0;
+        int j = nums.size() - 1;
+        int k = nums.size() - 1;        
+        while(i <= j) {
+            if(std::abs(nums[i]) >= std::abs(nums[j])) {
+                result[k] = pow(nums[i],2);                
+                i++;
+                k--;
+            } else {
+                result[k] = pow(nums[j],2);  
+                j--;
+                k--;
+            }   
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::cout << solution.sortedSquares({-4,-1,0,3,10}) << std::endl;
+}
